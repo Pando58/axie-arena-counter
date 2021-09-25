27c3008e09fb27684aa66871b71854d2be5c1140
Enrique Pando <enr.pando@gmail.com>
2021-09-25 11:22:52 -0500

    Reorganize layout

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,31 @@
 <template>
-  <div class="container mx-auto p-4">
-    <!-- Top Space -->
-  <div class="mt-20"></div>
+  <div class="container mx-auto h-screen p-4">
   
   <!-- Table -->
-  <ul class="mx-auto mt-4 max-w-4xl rounded-lg overflow-hidden shadow border-b border-gray-200 divide-y divide-gray-200 text-sm sm:text-base text-center font-medium text-gray-500">
-    <li class="py-3 flex divide-x divide-gray-200 bg-gray-50">
-      <div class="flex-shrink w-16 sm:w-24">Round</div>
-      <div class="flex-1">🟡 Energy</div>
-      <div class="flex-1">🃏 Cards</div>
-    </li>
-    <li
-      class="py-3 flex divide-x divide-gray-200"
-      v-for="round in rounds"
-      :key="round.round"
-    >
-      <div class="flex-shrink w-16 sm:w-24 text-gray-300" v-if="round.round === 0">Start</div>
-      <div class="flex-shrink w-16 sm:w-24" v-else>{{ round.round }}</div>
-      <div class="flex-1">{{ round.energy }}</div>
-      <div class="flex-1">{{ round.cards }}</div>
-    </li>
-  </ul>
+  <div class="h-full flex flex-col">
+    <div class="mt-24"></div>
+      <div class="flex-1">
+        <div class="mx-auto h-full max-w-4xl rounded-lg overflow-hidden shadow bg-white border-b border-gray-200 text-sm sm:text-base text-center font-medium text-gray-500">
+          <div class="py-3 flex divide-x divide-gray-200 bg-gray-50 border-b">
+            <div class="flex-shrink w-16 sm:w-24">Round</div>
+            <div class="flex-1">Energy</div>
+            <div class="flex-1">Cards</div>
+          </div>
+          <div class="divide-y divide-gray-200">
+            <li
+              class="py-3 flex divide-x divide-gray-200"
+              v-for="round in rounds"
+              :key="round.round"
+            >
+              <div class="flex-shrink w-16 sm:w-24 text-gray-300" v-if="round.round === 0">Start</div>
+              <div class="flex-shrink w-16 sm:w-24" v-else>{{ round.round }}</div>
+              <div class="flex-1">{{ round.energy }}</div>
+              <div class="flex-1">{{ round.cards }}</div>
+            </li>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
