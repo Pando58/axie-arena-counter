c57acdaf4006388fd07cccba30cdb608b2ab029b
Enrique Pando <enr.pando@gmail.com>
2021-09-25 17:25:21 -0500

    Fix tailwind purge html route

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
-  purge: ['./index.html', './src/**/*.{vue,js}'],
+  purge: ['./src/**/*.{html,vue,js}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
