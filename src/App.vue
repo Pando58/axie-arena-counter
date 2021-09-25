d61e252f21085645bb0c6e1b62a26d6c614c3907
Enrique Pando <enr.pando@gmail.com>
2021-09-25 10:11:46 -0500

    Main table layout

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,20 @@
 <template>
-  <div class="p-6 text-center text-2xl font-medium">Test</div>
+  <div class="container mx-auto p-4">
+    <!-- Top Space -->
+  <div class="mt-20"></div>
+  
+  <!-- Table -->
+  <ul class="mx-auto mt-4 max-w-4xl rounded-lg overflow-hidden shadow border-b border-gray-200 divide-y divide-gray-200 text-sm sm:text-base text-center font-medium text-gray-500">
+    <li class="py-3 flex divide-x divide-gray-200 bg-gray-50">
+      <div class="flex-shrink w-16 sm:w-24">Round</div>
+      <div class="flex-1">🟡 Energy</div>
+      <div class="flex-1">🃏 Cards</div>
+    </li>
+    <li class="py-3 flex divide-x divide-gray-200">
+      <div class="flex-shrink w-16 sm:w-24">Start</div>
+      <div class="flex-1">3</div>
+      <div class="flex-1">6</div>
+    </li>
+  </ul>
+  </div>
 </template>
